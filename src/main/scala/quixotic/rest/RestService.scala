03fe8b9ca31a3fd783bbb749b91d76f1a5bfe6af
Alexander Ioffe <deusaquilus@gmail.com>
2022-02-27 03:13:47 -0500

    fixing up zio2 stuff

--- src/main/scala/quixotic/rest/RestService.scala
+++ src/main/scala/quixotic/rest/RestService.scala
@@ -13,14 +13,14 @@ object RestService extends ZIOAppDefault:
   implicit val encoder: JsonEncoder[Record] = DeriveJsonEncoder.gen[Record]
 
   override def run =
-    ZIO.service[DataService].flatMap { dss =>
-      Server.start(
-        8088,
-        Http.collectZIO[Request] {
-          case Method.GET -> !! / "customers" =>
-            dss.getCustomers.map(cs => Response.json(cs.toJson))
-        }
-      )
-    }.provide(QuillContext.dataSourceLayer, DataService.live).exitCode
+    Server.start(
+      8088,
+      Http.collectZIO[Request] {
+        case Method.GET -> !! / "customers" =>
+          ZIO.environment[DataService].flatMap(dsl =>
+            dsl.get.getCustomers.map(cs => Response.json(cs.toJson))
+          )
+      }
+    ).provide(QuillContext.dataSourceLayer, DataService.live).exitCode
 
 end RestService
